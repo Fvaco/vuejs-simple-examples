24c1f0a331657c1e16c74ca04de483c13924062c
Iván Hernández Ortega <ivan@wupplier.com>
2018-04-05 00:55:47 +0100

    Example added
    - Custom directives example added

---
+++ Example4_CustomDirectives/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Challenge: Build a Custom Directive which works like v-on (Listen for Events) -->
+                <button v-my-on:click="" class="btn btn-default">Click me!</button>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        directives: {
+            myOn: {
+                bind(el, binding, vnode){
+                    if(binding.arg){
+                        el.addEventListener(binding.arg, ()=>{
+                            console.log(binding.arg);
+                        });
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
