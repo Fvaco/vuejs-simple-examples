0c1d78744f25ae31bbe2ac0522ab0611014be5df
Iván Hernández Ortega <ivan@wupplier.com>
2018-04-05 01:33:18 +0100

    Minor copywriting update

--- Example4_CustomDirectives/src/App.vue
+++ Example4_CustomDirectives/src/App.vue
@@ -4,11 +4,27 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Directives Exercise</h1>
                 <!-- Challenge: Build a Custom Directive which works like v-on (Listen for Events) -->
-                <button v-my-on:click="onFunction" class="btn btn-default">Click me!</button>
-                <div style="width: 100px; height: 100px;border: 1px solid #DEDEDE;border-bottom: 2px solid #DEDEDE; margin: 10px 0; background-color:whitesmoke"
-                     v-my-on:mousemove="onFunction">
+                <div id="eventSandbox"
+                     v-my-on:mouseenter="onFunction"
+                     v-my-on:mouseleave="onFunction"
+                     v-my-on:dblclick="onFunction"
+                     v-my-on:mousedown="onFunction"
+                     v-my-on:click="onFunction">
+                    {{lastEvent}}
                 </div>
-
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        You can perform this events over the gray box
+                    </div>
+                    <!--<div class="panel-body"></div>-->
+                    <div class="list-group">
+                        <div class="list-group-item">Click <span class="label label-default">click</span></div>
+                        <div class="list-group-item">Double click <span class="label label-default">dbclick</span></div>
+                        <div class="list-group-item">Mouse enter <span class="label label-default">mouseenter</span></div>
+                        <div class="list-group-item">Mouse leave <span class="label label-default">mouseleave</span></div>
+                        <div class="list-group-item">Mouse down <span class="label label-default">mousedown</span></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -16,6 +32,11 @@
 
 <script>
     export default {
+        data() {
+            return {
+                lastEvent: ''
+            }
+        },
         directives: {
             myOn: {
                 bind(el, binding, vnode) {
@@ -28,12 +49,25 @@
         },
         methods: {
             onFunction(e) {
-                console.log(e.type);
-
+                this.lastEvent = e.type;
             }
         }
     }
 </script>
 
-<style>
+<style scoped>
+    #eventSandbox {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: gray;
+        font-size: 1.3em;
+        width: 100%;
+        height: 100px;
+        border: 1px solid #DEDEDE;
+        border-bottom: 2px solid #DEDEDE;
+        margin: 10px 0;
+        background-color: whitesmoke;
+
+    }
 </style>
