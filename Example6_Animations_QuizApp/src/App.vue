172fa43c62eb83957c0c059154d4f879c4df51aa
Iván Hernández Ortega <ivan@wupplier.com>
2018-04-09 22:38:00 +0100

    Animations added

--- Example6_Animations_QuizApp/src/App.vue
+++ Example6_Animations_QuizApp/src/App.vue
@@ -8,8 +8,9 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-            
+                <transition name="flip" mode="out-in">
                     <component :is="mode" @answered="answered($event)" @confirmed="mode = 'appQuestion'"></component>
+                </transition>
             </div>
         </div>
     </div>
@@ -26,14 +27,14 @@
             }
         },
         methods: {
-          answered(isCorrect) {
-              if (isCorrect) {
-                  this.mode = 'appAnswer';
-              } else {
-                  this.mode = 'appQuestion';
-                  alert('Wrong, try again!');
-              }
-          }
+            answered(isCorrect) {
+                if (isCorrect) {
+                    this.mode = 'appAnswer';
+                } else {
+                    this.mode = 'appQuestion';
+                    alert('Wrong, try again!');
+                }
+            }
         },
         components: {
             appQuestion: Question,
@@ -41,4 +42,32 @@
         }
     }
 </script>
+<style scoped>
+
+    .flip-enter-active {
+        animation: flip-in .3s ease-in-out forwards;
+    }
+
+    .flip-leave-active {
+        animation: flip-out .3s ease-in-out forwards;
+    }
+
+    @keyframes flip-out {
+        from {
+            transform: rotateY(0deg);
+        }
+        to {
+            transform: rotateY(90deg);
+        }
+    }
+
+    @keyframes flip-in {
+        from {
+            transform: rotateY(90deg);
+        }
+        to {
+            transform: rotateY(0deg);
+        }
+    }
+</style>
 
